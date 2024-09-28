de14eb9795bfcec56274aa6439942dd4cf60428f
Dario Pareja <dpareja@nibbles.com>
2024-09-28 10:21:01 -0500

    Remove tracked folder and update .gitignore

--- Micro Project 2/web-app/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("colorButton").addEventListener("click", function() {
-    // Cambia el color de fondo a un color aleatorio
-    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
-});
